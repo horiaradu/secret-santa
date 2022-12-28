e32f565afbfc19d2993d9dfa52dbff0c3f1d3eab
Horia Radu <horia.radu@e-spres-oh.com>
2022-12-28 19:45:50 +0200

    Add progress bar on navigation events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import { useRouter } from 'next/router';
+
+NProgress.configure({ showSpinner: false });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      NProgress.start();
+    });
+    router.events.on('routeChangeComplete', () => {
+      NProgress.done();
+    });
+    router.events.on('routeChangeError', () => {
+      NProgress.done();
+    });
+  }, [router]);
+
   return (
     <div className="h-screen bg-white dark:bg-slate-800 dark:text-white">
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
       </Head>
       <body>
         <Main />
