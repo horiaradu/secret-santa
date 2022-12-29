387e1042fd1eebb2eea69a733df86b13d9638883
Horia Radu <horia.radu@e-spres-oh.com>
2022-12-29 14:48:57 +0200

    Anda new gift

--- components/anda.tsx
+++ components/anda.tsx
@@ -1,3 +1,16 @@
 export default function Anda() {
-  return <></>;
+  return (
+    <div className="container mx-auto px-5 flex flex-col gap-2 pt-5">
+      <h4 className="text-lg">
+        Aceasta este rezervarea pentru evenimentul ce va avea loc la The Wine
+        Guy, Emanoil Ungureanu 17, Timișoare în data de{' '}
+        <span className="text-bold uppercase">07.01.2023 – ora 20:00</span>.
+      </h4>
+      <p>
+        Pentru acest eveniment vă propunem o întâlnire mai lejeră, de
+        socializare. Vom avea 8 vinuri, din stiluri si colțuri diferite ale
+        lumii asociate cu tapas-uri faine și muzică pe vinyl.
+      </p>
+    </div>
+  );
 }
